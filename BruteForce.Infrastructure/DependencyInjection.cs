4da685a7944f98a6b99a2eec231e207473f72982
Mustafa Khazaee <nikmalabrahimkhil@gmail.com>
2023-11-21 14:02:10 +0430

    Changed SQL Server to PostgreSQL

--- BruteForce.Infrastructure/DependencyInjection.cs
+++ BruteForce.Infrastructure/DependencyInjection.cs
@@ -2,7 +2,6 @@
 using BruteForce.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using BruteForce.Infrastructure.Services;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BruteForce.Infrastructure;
@@ -13,13 +12,13 @@ public static class DependencyInjection
     /// Please provide your DbContext class in type parameter to register the DbContext and this method
     /// will provide the implementation for IRepository. Before calling this method
     /// you need to provide your own implementation for BruteForce.Domain.Interfaces.ICurrentUser
-    /// to enable auditing of your entities. Name your connection string as "DefaultConnection".
+    /// to enable auditing of your entities.
     /// </summary>
-    public static IServiceCollection AddInfrastructure<ApplicationDbContext> (this IServiceCollection services, IConfiguration configuration) where ApplicationDbContext : DbContext, IApplicationDbContext
+    public static IServiceCollection AddInfrastructure<ApplicationDbContext> (this IServiceCollection services, string connectionString) where ApplicationDbContext : DbContext, IApplicationDbContext
     {
         services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(config =>
             {
-                config.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                config.UseNpgsql(connectionString);
                 config.EnableDetailedErrors();
             }
             , ServiceLifetime.Scoped
